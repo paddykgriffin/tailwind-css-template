b5d4e035ab528e491d60c45952d2362f4cbd12ee
Paddy Griffin <paddygriffin@gmail.com>
2024-09-21 22:39:25 +0100

    Contact page layout change

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -5,8 +5,13 @@ interface Props {
 
 export default function ContactLayout({children}: Props) {
     return (
+        <>
+        <div className="hero h-screen">
+         Hero for contact page
+        </div>
         <div className="container mx-auto">
             {children}
         </div>
+        </>
     )
 }
